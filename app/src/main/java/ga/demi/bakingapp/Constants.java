6bf768c83b6786357c50c9c63e08f6e5544e676a
denis_mironov <d.mironov@rendez-vous.ru>
2020-04-02 13:41:43 +0300

    Created models and repository

--- app/src/main/java/ga/demi/bakingapp/Constants.java
+++ app/src/main/java/ga/demi/bakingapp/Constants.java
@@ -1,4 +1,9 @@
 package ga.demi.bakingapp;
 
-public class Constants {
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String BASE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
 }
